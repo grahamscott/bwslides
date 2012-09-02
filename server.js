707874cf01d749465c8edd094895f59d14a52b40
Graham Scott <ghound@gmail.com>
2012-09-02 12:05:39 +0100

    dropping 3.9 presentation

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var fs = require('fs'),
+	express = require('express'),
+	app = express.createServer();
+
+app.get('/', function(req, res){
+	fs.readFile(__dirname + '/public/index.html', 'utf8', function(err, text){
+        res.send(text);
+    });
+});
+
+app.configure('development', function(){
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+	app.use(express.static(__dirname + '/public'));
+	app.use(express.errorHandler());
+});
+
+app.listen(80);
